d309d6568605f82d57d71606742855e061b48d79
PhanKiet <tuankietvn@outlook.com>
2020-09-02 22:37:16 +0700

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,6 +28,7 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'recommonmark',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,3 +58,10 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+from recommonmark.parser import CommonMarkParser
+
+source_parsers = {'.md': CommonMarkParser}
+
+source_suffix = ['.rst', '.md']
